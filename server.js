cfecb74796a1b06cd6074d7460e9814f8b9a4ba3
utkarsh0498 <36528920+utkarsh0498@users.noreply.github.com>
2018-02-20 18:07:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req,res){
+    res.send('Article one requested and will be served here');
+});
+
 app.get('/article-two', function (req,res){
     res.send('Article two requested and will be served here');
 });
